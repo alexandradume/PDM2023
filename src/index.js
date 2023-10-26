9913d6a33effd01b60e9b8edbaeab3d426404b13
alexandradume <alexandra.teodora.dume@stud.ubbcluj.ro>
2023-10-26 15:40:30 +0300

    basic server

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ app.use(async (ctx, next) => {
 });
 
 app.use(async (ctx, next) => {
-  await new Promise(resolve => setTimeout(resolve, 2000));
+  await new Promise(resolve => setTimeout(resolve, 5000));
   await next();
 });
 
@@ -30,21 +30,25 @@ app.use(async (ctx, next) => {
   }
 });
 
-class Item {
-  constructor({ id, text, date, version }) {
+class Restaurant {
+  constructor({ id, name,stars, date, version }) {
     this.id = id;
-    this.text = text;
-    this.date = date;
+    this.name = name;
+    this.stars = stars;
     this.version = version;
   }
 }
 
-const items = [];
+const restaurants = [];
 for (let i = 0; i < 3; i++) {
-  items.push(new Item({ id: `${i}`, text: `item ${i}`, date: new Date(Date.now() + i), version: 1 }));
+  restaurants
+.push(new Restaurant
+  ({ id: `${i}`, name: `restaurant ${i}`,stars: `stars ${i}`, date: new Date(Date.now() + i), version: 1 }));
 }
-let lastUpdated = items[items.length - 1].date;
-let lastId = items[items.length - 1].id;
+let lastUpdated = restaurants[restaurants
+.length - 1].date;
+let lastId = restaurants[restaurants
+.length - 1].id;
 const pageSize = 10;
 
 const broadcast = data =>
@@ -56,103 +60,95 @@ const broadcast = data =>
 
 const router = new Router();
 
-router.get('/item', ctx => {
-  const ifModifiedSince = ctx.request.get('If-Modif ied-Since');
-  if (ifModifiedSince && new Date(ifModifiedSince).getTime() >= lastUpdated.getTime() - lastUpdated.getMilliseconds()) {
-    ctx.response.status = 304; // NOT MODIFIED
-    return;
-  }
-  const text = ctx.request.query.text;
-  const page = parseInt(ctx.request.query.page) || 1;
-  ctx.response.set('Last-Modified', lastUpdated.toUTCString());
-  const sortedItems = items
-    .filter(item => text ? item.text.indexOf(text) !== -1 : true)
-    .sort((n1, n2) => -(n1.date.getTime() - n2.date.getTime()));
-  const offset = (page - 1) * pageSize;
-  // ctx.response.body = {
-  //   page,
-  //   items: sortedItems.slice(offset, offset + pageSize),
-  //   more: offset + pageSize < sortedItems.length
-  // };
-  ctx.response.body = items;
+router.get('/restaurants', ctx => {
+  ctx.response.body = restaurants
+;
   ctx.response.status = 200;
 });
 
-router.get('/item/:id', async (ctx) => {
-  const itemId = ctx.request.params.id;
-  const item = items.find(item => itemId === item.id);
-  if (item) {
-    ctx.response.body = item;
+router.get('/restaurant/:id', async (ctx) => {
+  const restaurantId = ctx.request.params.id;
+  const restaurant = restaurants
+.find(restaurant => restaurantId === restaurant.id);
+  if (restaurant) {
+    ctx.response.body = restaurant;
     ctx.response.status = 200; // ok
   } else {
-    ctx.response.body = { issue: [{ warning: `item with id ${itemId} not found` }] };
+    ctx.response.body = { message: `restaurant with id ${restaurantId} not found` };
     ctx.response.status = 404; // NOT FOUND (if you know the resource was deleted, then return 410 GONE)
   }
 });
 
 const createItem = async (ctx) => {
-  const item = ctx.request.body;
-  if (!item.text) { // validation
-    ctx.response.body = { issue: [{ error: 'Text is missing' }] };
+  const restaurant = ctx.request.body;
+  if (!restaurant.name) { // validation
+    ctx.response.body = { message: 'Name is missing' };
     ctx.response.status = 400; //  BAD REQUEST
     return;
   }
-  item.id = `${parseInt(lastId) + 1}`;
-  lastId = item.id;
-  item.date = new Date();
-  item.version = 1;
-  items.push(item);
-  ctx.response.body = item;
+  restaurant.id = `${parseInt(lastId) + 1}`;
+  lastId = restaurant.id;
+  restaurant.date = new Date();
+  restaurant.version = 1;
+  restaurants
+.push(restaurant);
+  ctx.response.body = restaurant;
   ctx.response.status = 201; // CREATED
-  broadcast({ event: 'created', payload: { item } });
+  broadcast({ event: 'created', payload: { restaurant } });
 };
 
-router.post('/item', async (ctx) => {
+router.post('/restaurant', async (ctx) => {
   await createItem(ctx);
 });
 
-router.put('/item/:id', async (ctx) => {
+router.put('/restaurant/:id', async (ctx) => {
   const id = ctx.params.id;
-  const item = ctx.request.body;
-  item.date = new Date();
-  const itemId = item.id;
-  if (itemId && id !== item.id) {
-    ctx.response.body = { issue: [{ error: `Param id and body id should be the same` }] };
+  const restaurant = ctx.request.body;
+  restaurant.date = new Date();
+  const restaurantId = restaurant.id;
+  if (restaurantId && id !== restaurant.id) {
+    ctx.response.body = { message: `Param id and body id should be the same` };
     ctx.response.status = 400; // BAD REQUEST
     return;
   }
-  if (!itemId) {
+  if (!restaurantId) {
     await createItem(ctx);
     return;
   }
-  const index = items.findIndex(item => item.id === id);
+  const index = restaurants
+.findIndex(restaurant => restaurant.id === id);
   if (index === -1) {
-    ctx.response.body = { issue: [{ error: `item with id ${id} not found` }] };
+    ctx.response.body = { issue: [{ error: `restaurant with id ${id} not found` }] };
     ctx.response.status = 400; // BAD REQUEST
     return;
   }
-  const itemVersion = parseInt(ctx.request.get('ETag')) || item.version;
-  if (itemVersion < items[index].version) {
+  const itemVersion = parseInt(ctx.request.get('ETag')) || restaurant.version;
+  if (itemVersion < restaurants
+  [index].version) {
     ctx.response.body = { issue: [{ error: `Version conflict` }] };
     ctx.response.status = 409; // CONFLICT
     return;
   }
-  item.version++;
-  items[index] = item;
+  restaurant.version++;
+  restaurants
+[index] = restaurant;
   lastUpdated = new Date();
-  ctx.response.body = item;
+  ctx.response.body = restaurant;
   ctx.response.status = 200; // OK
-  broadcast({ event: 'updated', payload: { item } });
+  broadcast({ event: 'updated', payload: { restaurant } });
 });
 
-router.del('/item/:id', ctx => {
+router.del('/restaurant/:id', ctx => {
   const id = ctx.params.id;
-  const index = items.findIndex(item => id === item.id);
+  const index = restaurants
+.findIndex(restaurant => id === restaurant.id);
   if (index !== -1) {
-    const item = items[index];
-    items.splice(index, 1);
+    const restaurant = restaurants
+  [index];
+    restaurants
+  .splice(index, 1);
     lastUpdated = new Date();
-    broadcast({ event: 'deleted', payload: { item } });
+    broadcast({ event: 'deleted', payload: { restaurant } });
   }
   ctx.response.status = 204; // no content
 });
@@ -160,12 +156,13 @@ router.del('/item/:id', ctx => {
 setInterval(() => {
   lastUpdated = new Date();
   lastId = `${parseInt(lastId) + 1}`;
-  const item = new Item({ id: lastId, text: `item ${lastId}`, date: lastUpdated, version: 1 });
-  items.push(item);
-  console.log(`
-   ${item.text}`);
-  broadcast({ event: 'created', payload: { item } });
-}, 150000);
+  const restaurant = new Restaurant
+({ id: lastId, name: `name ${lastId}`,stars: `stars ${lastId}`, date: lastUpdated, version: 1 });
+  restaurants
+.push(restaurant);
+  console.log(`New restaurant: ${restaurant.name} ${restaurant.stars} `);
+  broadcast({ event: 'created', payload: { restaurant } });
+}, 5000);
 
 app.use(router.routes());
 app.use(router.allowedMethods());
